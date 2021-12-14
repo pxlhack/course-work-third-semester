e5c0f7c05cc206de29dad5678dc73b3d08925127
F <nellsouhd@gmail.com>
2021-12-14 20:17:18 +0700

    first-commit

---
+++ cmake-build-debug/CMakeFiles/courseWork.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/courseWork.dir/main.cpp.obj"
+  "courseWork.exe"
+  "courseWork.exe.manifest"
+  "courseWork.pdb"
+  "libcourseWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/courseWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
